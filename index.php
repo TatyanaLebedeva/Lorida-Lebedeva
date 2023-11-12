cc3abc62c3fc7659059c2281a798c351e5ae5874
FivePood <ps220790@gmail.com>
2023-11-12 19:00:41 +0300

    add calc and databases

--- index.php
+++ index.php
@@ -1,33 +1,60 @@
 <?php
-if (isset($_REQUEST['number1']) && isset($_REQUEST['number2'])) {
-    if (!is_numeric($_REQUEST['number1'])) {
-        echo "Первое значение не является числом!\n";
+function connectToDB()
+{
+    $mysqli = new mysqli("localhost", "root", "", "calculator");
+    if ($mysqli->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+        exit();
     }
-    if (!is_numeric($_REQUEST['number2'])) {
-        echo "Второе значение не является числом!\n";
-    }
-    if (is_numeric($_REQUEST['number1']) && is_numeric($_REQUEST['number2'])) {
-        echo(calc($_REQUEST['number1'], $_REQUEST['number2'], $_REQUEST['operation']));
-    }
-    }
+    return $mysqli;
+}
+
 function calc($number1, $number2, $operation)
 {
     $result = '';
     if ($operation === 'addition') {
+        $action = '+';
         $result = $number1 + $number2;
+        addAction($number1, $action, $number2, $result);
     } else if ($operation === 'subtraction') {
+        $action = '-';
         $result = $number1 - $number2;
+        addAction($number1, $action, $number2, $result);
     } else if ($operation === 'multiplication') {
+        $action = '*';
         $result = $number1 * $number2;
+        addAction($number1, $action, $number2, $result);
     } else if ($operation === 'division') {
         if ($number2 == 0) {
-            $result = 'на 0 делить нельзя';
+            $result = 'На 0 делить нельзя';
         } else {
+            $action = '/';
             $result = $number1 / $number2;
+            addAction($number1, $action, $number2, $result);
         }
     }
     return $result;
 }
+
+function addAction($number1, $action, $number2, $result)
+{
+    $connection = connectToDB();
+    $sql = "INSERT INTO calc (number_one, operation, number_two, result) values ($number1, '$action', $number2, $result)";
+    $connection->query($sql);
+}
+
+function getListAction()
+{
+    $connection = connectToDB();
+    $res = $connection->query("select number_one, operation, number_two, result 
+                                        from calc 
+                                        order by id desc
+                                        limit 7");
+    foreach ($res as $value) {
+        echo "{$value['number_one']} {$value['operation']} {$value['number_two']} = {$value['result']} <br>";
+    }
+}
+
 ?>
 <html lang="ru">
 <head>
@@ -60,5 +87,20 @@ function calc($number1, $number2, $operation)
         <input type="submit" value="Вычислить">
     </div>
 </form>
+<?php
+if (isset($_REQUEST['number1']) && isset($_REQUEST['number2'])) {
+    if (!is_numeric($_REQUEST['number1'])) {
+        echo "Первое значение не является числом!<br>";
+    }
+    if (!is_numeric($_REQUEST['number2'])) {
+        echo "Второе значение не является числом!<br>";
+    }
+    if (is_numeric($_REQUEST['number1']) && is_numeric($_REQUEST['number2'])) {
+        echo "Результат: " . calc($_REQUEST['number1'], $_REQUEST['number2'], $_REQUEST['operation']) . '<br>';
+    }
+}
+echo 'Последние вычисления: <br>';
+getListAction();
+?>
 </body>
 </html>
